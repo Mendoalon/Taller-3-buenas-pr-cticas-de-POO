2762d17c50979195c5d74d64c536f8e3abe85430
Luis Mendoza <luis.mendoza.alonso@hotmail.com>
2023-02-27 11:59:28 -0500

    Se crean clases e interface a usar.

---
+++ src/taller/pkg3/buenas/pr/cticas/de/poo/MainLibraryInteface.java
@@ -0,0 +1,10 @@
+
+package taller.pkg3.buenas.pr.cticas.de.poo;
+
+/**
+ *
+ * @author Luis mendoza
+ */
+public interface MainLibraryInteface {
+    
+}
---
+++ src/taller/pkg3/buenas/pr/cticas/de/poo/Taller3BuenasPrCticasDePOO.java
@@ -0,0 +1,17 @@
+
+package taller.pkg3.buenas.pr.cticas.de.poo;
+
+/**
+ *
+ * @author Luis mendoza
+ */
+public class Taller3BuenasPrCticasDePOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
